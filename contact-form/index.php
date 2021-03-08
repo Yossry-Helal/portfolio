c2fe0ff8ea807767b916c65d87a80bf3ef1ae430
cairotx <cairotx@gmail.com>
2021-03-07 21:45:38 -0600

    first commit

---
+++ contact-form/index.php
@@ -0,0 +1,37 @@
+<div class="container">
+        <div class="contact-header">
+            <h1 class="text-center pt-5 text-dark text-capitalize">Contact Me</h1>
+            <p class="leader fs-6 pt-4 text-dark text-center d-block text-info ">you can contact me at any time and I will contact you back ASAP</p>
+        </div>
+        <form method="POST" class="text-dark" action="process.php">
+            <div class="form-group">
+                <label for="firstname">First Name</label>
+                <input type="text" name="firstname" class="form-control" id="firstname">
+            </div>
+            <div class="form-group">
+                <label for="firstname">Last Name</label>
+                <input type="text" name="lastname" class="form-control" id="lastname" required>
+            </div>
+            <div class="form-group">
+                <label for="firstname">Phone</label>
+                <input type="text" name="phone" class="form-control" id="phone" required>
+            </div>
+            <div class="form-group">
+                <label for="firstname">Email</label>
+                <input type="email" name="email" class="form-control" id="email" required>
+            </div>
+            <div class="form-group">
+                <label for="message">message</label>
+                <input type="text" name="message" class="form-control" id="message" required>
+            </div>
+            <div class="form-group text-center">
+                <button type="submit" class="btn btn-primary mt-4 fw-bolder mb-4 text-capitalize" required>Submit</button>
+                
+            </div>
+
+
+
+        </form>
+
+    </div>
+
